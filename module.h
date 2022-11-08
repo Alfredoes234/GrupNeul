d2421cbdf8ca1f7585b521b33940dfa9087e3255
Alfredoes234 <114923333+Alfredoes234@users.noreply.github.com>
2022-11-08 10:18:58 +0000

    Create module.h
    Allows for individual Modules

---
+++ module.h
@@ -0,0 +1,43 @@
+#include <assert.h>
+#include <float.h>
+#include <math.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+#include <setjmp.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <locale.h>
+#include <signal.h>
+#include <stdio.h>
+#include <time.h>
+
+/////////////////////////////
+
+#ifndef module_clr
+#define module_clr
+
+// clear module
+
+void clr() {
+    system("clear");
+};
+
+#endif /* module_clr */
+
+/////////////////////////////
+
+#ifndef module_ad
+#define module_ad
+
+// addition Module
+
+void add() {
+  printf("\nPain");
+};
+
+#endif
+
+/////////////////////////////
